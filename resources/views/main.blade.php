08dd93726b649988538818079293dac250de4c3b
Lucas Guilherme <lucassguilherme159@gmail.com>
2026-03-16 11:04:13 -0400

    Views e Blade

---
+++ resources/views/main.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1> Welcome View and Blade!</h1>
+    <hr>
+    <h3> The valus is: {{ $value }} </h3>
+    <h3> The value is: <?= $value ?></h3>
+    <h3> The value is: <?php echo $value ?></h3>
+    
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/main/{value}', function ($value) {
+    return view('main', ['value' => $value]);
 });
