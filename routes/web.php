fbb0624c66759c31c03b952d2502ba48180c1d92
Lucas Guilherme <lucassguilherme159@gmail.com>
2026-03-16 11:29:55 -0400

    Criando Layout para múltiplas views

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
+
+Route::get('/', function () {
+    echo "Hello World!";
+});
+
+Route::get('/about', function (){
+    echo 'About us';
+});
 
 Route::get('/main/{value}', function ($value) {
     return view('main', ['value' => $value]);
 });
+
+Route::get('/main/{value}', [MainController::class, 'index']);
+Route::get('/page2/{value}', [MainController::class, 'page2']);
+Route::get('/page3/{value}', [MainController::class, 'page3']);
+
+
+
+
+
