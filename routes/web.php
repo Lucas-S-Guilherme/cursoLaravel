c395fa1de7846afbeac9832cdf7ed3c68e3af4fc
Lucas Guilherme <lucassguilherme159@gmail.com>
2026-03-17 17:49:00 -0400

    Controller para autenticação

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
+use App\Http\Controllers\AuthController;
+// Auth routes
 
+Route::get('/login', [AuthController::class, 'login']);
+Route::get('/logout', [AuthController::class, 'logout']);
 
 
 
