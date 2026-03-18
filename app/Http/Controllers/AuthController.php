f48bed49d2e708fa4ce6159d2b19f121eba01ab3
Lucas Guilherme <lucassguilherme159@gmail.com>
2026-03-17 21:45:07 -0400

    Conectando ao DB

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
 
 class AuthController extends Controller
 {
@@ -38,8 +40,16 @@ class AuthController extends Controller
 
         $username = $request->input('text_username');
         $password = $request->input('text_password');
+        
+        // test database connection
+        try {           
+            DB::connection()->getPdo();
+            echo 'Connection is OK!';
+        } catch (\PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
 
-        echo 'OK!';
+        echo "FIM";
       
     }
     
