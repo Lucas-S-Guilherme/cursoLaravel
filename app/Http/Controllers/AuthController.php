db88ae004315ad7a7b8e11daa28f6c89e04670e3
Lucas Guilherme <lucassguilherme159@gmail.com>
2026-03-17 20:12:23 -0400

    Introdução à validação de Dados

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -18,9 +18,20 @@ class AuthController extends Controller
     
     public function loginSubmit(Request $request)
     {
-        echo $request->input('text_username');
-        echo '<br>';
-        echo $request->input('text_password');
+        // form validation
+        $request->validate(
+            [
+                'text_username' => 'required',
+                'text_password' => 'required',
+                
+            ]
+        ); 
+
+        $username = $request->input('text_username');
+        $password = $request->input('text_password');
+
+        echo 'OK!';
+      
     }
     
 }
