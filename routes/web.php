1ef2b7ab12cbbfc1ff2b36fe00c1c4ac3d56673a
Lucas Guilherme <lucassguilherme159@gmail.com>
2026-03-25 21:01:22 -0400

    Middleware adicional para usuário já está logado

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,25 @@
 <?php
 
 use App\Http\Middleware\CheckIsLogged;
+use App\Http\Middleware\CheckIsNotLogged;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\AuthController;
 
-// Auth routes
-Route::get('/login', [AuthController::class, 'login']);
-Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
+// Auth routes - users not logged
+Route::middleware([CheckIsNotLogged::class])->group(function(){
+    Route::get('/login', [AuthController::class, 'login']);
+    Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
+});
 
+// app routes - user logged
 Route::middleware([CheckIsLogged::class])->group(function(){
     Route::get('/', [MainController::class, 'index']);
     Route::get('/newNote', [MainController::class, 'newNote']);
     Route::get('/logout', [AuthController::class, 'logout']);
 });
+
+
 
 
 
