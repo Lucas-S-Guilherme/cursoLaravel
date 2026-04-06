43e7ba69b1f0fab98712a906502c6e35db2ec103
Lucas Guilherme <lucassguilherme159@gmail.com>
2026-04-06 15:10:45 -0400

    Vamos optimizr o código

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -26,23 +26,29 @@ class MainController extends Controller
 
     public function editNote($id)
     {
-        try {
-            $id = Crypt::decrypt($id);
-        } catch (DecryptException $e) {
-            return redirect()->route('home');
-        }
+        $id = $this->decryptId($id); 
         echo "I'm editing note with id = $id"; 
     }
 
 
     public function deleteNote($id)
     {
+        $id = $this->decryptId($id); 
+        echo "I'm deleting note with id = $id";
+    }
+
+    private function decryptID($id)
+    {
         try {
             $id = Crypt::decrypt($id);
         } catch (DecryptException $e) {
             return redirect()->route('home');
         }
-        echo "I'm deleting note with id = $id"; 
+            
+        return $id;
     }
+
+    
+
 
 }
