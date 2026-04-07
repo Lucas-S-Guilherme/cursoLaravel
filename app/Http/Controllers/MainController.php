40be8a7bf28d7da747210cc51c59f1ffff60704e
Lucas Guilherme <lucassguilherme159@gmail.com>
2026-04-07 12:00:18 -0400

    Outra Forma de Soft Delete - Finalizado o Projeto

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -146,8 +146,14 @@ class MainController extends Controller
         // $note->delete();
 
         // 2. soft delete
-        $note->deleted_at = date('Y:m:d H:i:s');
-        $note->save();
+        // $note->deleted_at = date('Y:m:d H:i:s');
+        // $note->save();
+
+        // 3. soft delete (property in model)
+        $note->delete();
+
+        // 4. hard delete (property SoftDeletes in model)
+        // $note->forceDelete();
 
         return redirect()->route('home');
     }
