f75dc4f14fd05a9c497ed6282ef839e3cd8ffd5b
Lucas Guilherme <lucassguilherme159@gmail.com>
2026-04-07 11:39:54 -0400

    deletando uma nota - hard delete e soft delete

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,9 @@ Route::middleware([CheckIsLogged::class])->group(function(){
     Route::get('/editNote/{id}', [MainController::class, 'editNote'])->name('edit');
     Route::post('/editNoteSubmit', [MainController::class, 'editNoteSubmit'])->name('editNoteSubmit');
 
+    // delete note
     Route::get('/deleteNote/{id}', [MainController::class, 'deleteNote'])->name('delete');
+    Route::get('/deleteNoteConfirm/{id}', [MainController::class, 'deleteNoteConfirm'])->name('deleteConfirm');    
    
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 });
